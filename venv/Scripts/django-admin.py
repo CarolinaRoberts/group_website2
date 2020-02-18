59636732102cf65fae34d609514b350d33d0ec19
Raphael Ramos <ramosri@cardiff.ac.uk>
2020-02-18 14:12:02 +0000

    deleted venv folder

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\computer science\compsci_y2\groupproject\website\environments\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
